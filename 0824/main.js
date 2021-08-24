f901de4362baae3415a90f000ecf84ba5af0cdfd
森田彩月 <74756473+satuki1124@users.noreply.github.com>
2021-08-25 00:13:39 +0900

    Add files via upload

---
+++ 0824/main.js
@@ -0,0 +1,152 @@
+//logoの表示
+$(window).on('load',function(){
+  $("#splash").delay(1500).fadeOut('slow');//ローディング画面を1.5秒（1500ms）待機してからフェードアウト
+  $("#splash_logo").delay(1200).fadeOut('slow');//ロゴを1.2秒（1200ms）待機してからフェードアウト
+});
+jQuery(document).ready(function( $ ) {
+  // 桜の花
+  $('#toggle').click(function() {
+    $(this).toggleClass('active');
+    $('#overlay').toggleClass('open');
+   });
+    // Init Fancybox
+    jQuery(".fancybox").fancybox();
+
+    // Init Stellar
+    $.stellar({
+      horizontalScrolling: false,
+      verticalOffset: 40
+    });
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+  // コンテナを指定
+  const section = document.querySelector('.cherry-blossom-container');
+
+  // 花びらを生成する関数
+  const createPetal = () => {
+    const petalEl = document.createElement('span');
+    petalEl.className = 'petal';
+    const minSize = 10;
+    const maxSize = 15;
+    const size = Math.random() * (maxSize + 1 - minSize) + minSize;
+    petalEl.style.width = `${size}px`;
+    petalEl.style.height = `${size}px`;
+    petalEl.style.left = Math.random() * innerWidth + 'px';
+    section.appendChild(petalEl);
+
+    // 一定時間が経てば花びらを消す
+    setTimeout(() => {
+      petalEl.remove();
+    }, 10000);
+  }
+
+  // 花びらを生成する間隔をミリ秒で指定
+  setInterval(createPetal, 300);
+});
+//スクロールすると上部に固定させるための設定を関数でまとめる
+function FixedAnime() {
+	var headerH = $('#header').outerHeight(true);
+	var scroll = $(window).scrollTop();
+	if (scroll >= headerH){//headerの高さ以上になったら
+			$('#header').addClass('fixed');//fixedというクラス名を付与
+		}else{//それ以外は
+			$('#header').removeClass('fixed');//fixedというクラス名を除去
+		}
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+	FixedAnime();/* スクロール途中からヘッダーを出現させる関数を呼ぶ*/
+});
+
+// ページが読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+	FixedAnime();/* スクロール途中からヘッダーを出現させる関数を呼ぶ*/
+});
+// お知らせが画像を動かす
+function delayScrollAnime() {
+	var time = 0.2;//遅延時間を増やす秒数の値
+	var value = time;
+	$('.delayScroll').each(function () {
+		var parent = this;					//親要素を取得
+		var elemPos = $(this).offset().top;//要素の位置まで来たら
+		var scroll = $(window).scrollTop();//スクロール値を取得
+		var windowHeight = $(window).height();//画面の高さを取得
+		var childs = $(this).children();	//子要素を取得
+		
+		if (scroll >= elemPos - windowHeight && !$(parent).hasClass("play")) {//指定領域内にスクロールが入ったらまた親要素にクラスplayがなければ
+			$(childs).each(function () {
+				
+				if (!$(this).hasClass("fadeUp")) {//アニメーションのクラス名が指定されているかどうかをチェック
+					
+					$(parent).addClass("play");	//親要素にクラス名playを追加
+					$(this).css("animation-delay", value + "s");//アニメーション遅延のCSS animation-delayを追加し
+					$(this).addClass("fadeUp");//アニメーションのクラス名を追加
+					value = value + time;//delay時間を増加させる
+					
+					//全ての処理を終わったらplayを外す
+					var index = $(childs).index(this);
+					if((childs.length-1) == index){
+						$(parent).removeClass("play");
+					}
+				}
+			})
+		}else {
+			$(childs).removeClass("fadeUp");//アニメーションのクラス名を削除
+			value = time;//delay初期値の数値に戻す
+		}
+	})
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+	$(window).scroll(function (){
+		delayScrollAnime();/* アニメーション用の関数を呼ぶ*/
+	});// ここまで画面をスクロールをしたら動かしたい場合の記述
+
+// 画面が読み込まれたらすぐに動かしたい場合の記述
+	$(window).on('load', function(){
+		delayScrollAnime();/* アニメーション用の関数を呼ぶ*/
+	});// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+	function slideAnime(){
+  //====左に動くアニメーションここから===
+    $('.leftAnime').each(function(){ 
+      var elemPos = $(this).offset().top-50;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll >= elemPos - windowHeight){
+        //左から右へ表示するクラスを付与
+        //テキスト要素を挟む親要素（左側）とテキスト要素を元位置でアニメーションをおこなう
+        $(this).addClass("slideAnimeLeftRight"); //要素を左枠外にへ移動しCSSアニメーションで左から元の位置に移動
+        $(this).children(".leftAnimeInner").addClass("slideAnimeRightLeft");  //子要素は親要素のアニメーションに影響されないように逆の指定をし元の位置をキープするアニメーションをおこなう
+      }else{
+        //左から右へ表示するクラスを取り除く
+        $(this).removeClass("slideAnimeLeftRight");
+        $(this).children(".leftAnimeInner").removeClass("slideAnimeRightLeft");
+        
+      }
+    });
+    
+  }
+  
+  // 画面をスクロールをしたら動かしたい場合の記述
+  $(window).scroll(function (){
+    slideAnime();/* アニメーション用の関数を呼ぶ*/
+  });// ここまで画面をスクロールをしたら動かしたい場合の記述
+
+  // 画面が読み込まれたらすぐに動かしたい場合の記述
+  $(window).on('load', function(){
+    slideAnime();/* アニメーション用の関数を呼ぶ*/
+  });// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+  $('.slider').slick({
+	autoplay: true,//自動的に動き出すか。初期値はfalse。
+	infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+	speed: 500,//スライドのスピード。初期値は300。
+	slidesToShow: 3,//スライドを画面に3枚見せる
+	slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
+	prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+	nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+	centerMode: true,//要素を中央ぞろえにする
+	variableWidth: true,//幅の違う画像の高さを揃えて表示
+	dots: true,//下部ドットナビゲーションの表示
+});
